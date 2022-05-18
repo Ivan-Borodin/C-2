8ccb90b5aedb3c9481f3e2244b0d91cf4b1420de
Ivan <ivan241578@gmail.com>
2022-05-18 20:28:02 +0300

    Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите цифру от 1 до 7: ");
+int a = Convert.ToInt32(Console.ReadLine());
+while (a < 1 || a > 7)
+{
+    Console.WriteLine("Введите цифру от 1 до 7: ");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine(a == 6 || a == 7 ? "да" : "нет");
