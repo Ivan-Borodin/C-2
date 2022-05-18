29312b4e0cf861c557d63920947e5cdf47a6185d
Ivan <ivan241578@gmail.com>
2022-05-18 21:24:41 +0300

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 100) Console.WriteLine("Третьей цифры нет ");
+else if (a > 99 && a < 1000)
+{
+    int rez = a % 10;
+    Console.WriteLine(rez);
+}
+else if (a > 999 && a < 10000)
+{
+    int rez = a / 10 % 10;
+    Console.WriteLine(rez);
+}
+else if (a > 9999 && a < 100000)
+{
+    int rez = a / 100 % 10;
+    Console.WriteLine(rez);
+}
+else if (a > 99999 && a < 1000000)
+{
+    int rez = a / 1000 % 10;
+    Console.WriteLine(rez);
+}
+else if (a > 999999 && a < 10000000)
+{
+    int rez = a / 10000 % 10;
+    Console.WriteLine(rez);
+}
+else Console.WriteLine("Слишком большое число");
