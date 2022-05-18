ebec57e680fb4af089915e36764f4e7e2dc78a48
Ivan <ivan241578@gmail.com>
2022-05-18 20:06:36 +0300

    Task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+while (a<100 || a >999)
+{
+    Console.WriteLine("Введите трехзначное число: ");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+int number2 = a/10%10;
+Console.WriteLine($"2 цифра = {number2}");
